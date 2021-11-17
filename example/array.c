c01297dda4179204015999adcb7d22089bd56636
joel <bin3340@163.com>
2021-11-17 22:59:01 +0800

    Add files via upload

---
+++ example/array.c
@@ -0,0 +1,141 @@
+/*
+ * 
+ * Copyright (c) 2021, Joel
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/time.h>
+#include <assert.h>
+
+#include "utils.h"
+#include "skiplist.h"
+
+#define ARRAY_OK 0
+#define ARRAY_ERR -1
+
+struct array_item{
+    int index;
+    char *value;
+};
+
+struct array{
+    struct skiplist *sl;
+};
+
+static int array_item_cmp(void *k1, void *k2){
+    struct array_item *i = (struct array_item *)k1, *j = (struct array_item *)k2;
+    return i->index - j->index;
+}
+
+static void array_item_free(struct array_item *item){
+    if(NULL != item){
+        if(NULL != item->value)
+            free(item->value);
+        free(item);
+    }
+}
+
+struct array *array_create(){
+    struct array *a = NULL;
+    struct skiplist *sl = NULL;
+
+    a = calloc(1, sizeof(*a));
+    if(NULL == a) goto err;
+    sl = skiplist_create(array_item_cmp);
+    if(NULL == sl) goto err;
+    a->sl = sl;
+    return a;
+
+err:
+    if(NULL != sl)
+        skiplist_free(sl);
+    if(NULL != a)
+        free(a);
+    return NULL;
+}
+
+int array_del(struct array *a, int index){
+    struct array_item item = { .index=index, .value=NULL };
+    struct array_item *predeleted_item = skiplist_remove(a->sl, (void *)&item);
+    if(NULL != predeleted_item){
+        array_item_free(predeleted_item);
+        return ARRAY_OK;
+    }
+    return ARRAY_ERR;
+}
+
+struct array_item *array_get(struct array *a, int index){
+    struct array_item item = { .index=index, .value=NULL };
+    return skiplist_search(a->sl, (void *)&item);
+}
+
+int array_set(struct array *a, struct array_item *item){
+    array_del(a, item->index); //delete before insert
+    return SKIPLIST_OK == skiplist_insert(a->sl, (void *)item) ? ARRAY_OK : ARRAY_ERR;
+}
+
+void array_free(struct array *a){
+    if(NULL == a) return;
+    SKIPLIST_FOREACH(a->sl, (void *curr){
+        struct array_item *item = (struct array_item *)curr;
+        //printf("index:%d value:%s\n", item->index, item->value);        
+        array_del(a, item->index);
+    });
+    skiplist_free(a->sl);
+    free(a);
+}
+
+///////////////////////////////////////////////////////////////////////////////
+
+void stress_testing(struct array *a, int data_len, int count) __attribute__((unused));
+void stress_testing(struct array *a, int data_len, int count) {
+    int64_t start = getCurrentTime();
+    int i = 0;
+    for(; i < count; i++){
+        struct array_item *item = calloc(1, sizeof(*item));
+        item->index = i;
+        item->value = random_str(data_len);
+        assert(ARRAY_OK == array_set(a, (void *)item));
+        assert(item == array_get(a, item->index));
+    }
+    printf("time consuming:%ld data_len:%d count:%d\n", getCurrentTime() - start, data_len, count);
+}
+
+void cover_testing(struct array *a) __attribute__((unused));
+void cover_testing(struct array *a) {
+    char *new_key = calloc(1, 6);
+    strcpy(new_key, "test2");
+    struct array_item *new_item = calloc(1, sizeof(*new_item));
+    new_item->index = 0;
+    new_item->value = new_key;
+    assert(ARRAY_OK == array_set(a, (void *)new_item));
+    assert(new_item == array_get(a, 0));
+}
+
+int main(){
+    struct array *a = array_create();
+
+    stress_testing(a, 32, 100000);
+    cover_testing(a);
+
+    //free
+    printf("array size before free:%d\n", a->sl->busy);
+    array_free(a);
+
+    printf("over\n");
+    return 0;
+}
---
+++ example/map.c
@@ -0,0 +1,157 @@
+/*
+ * 
+ * Copyright (c) 2021, Joel
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/time.h>
+#include <assert.h>
+
+#include "utils.h"
+#include "skiplist.h"
+
+#define MAP_OK 0
+#define MAP_ERR -1
+#define MAP_MAX_KEY_LEN 32
+
+struct map_pair{
+    char *key;
+    char *value;
+};
+
+struct map{
+    struct skiplist *sl;
+};
+
+static int map_pair_cmp(void *k1, void *k2){
+    struct map_pair *i = (struct map_pair *)k1, *j = (struct map_pair *)k2;
+    return strncmp(i->key, j->key, MAP_MAX_KEY_LEN);
+}
+
+void map_pair_free(struct map_pair *pair){
+    if(NULL != pair){
+        if(NULL != pair->key)
+            free(pair->key);
+        if(NULL != pair->value)
+            free(pair->value);
+        free(pair);
+    }
+}
+
+struct map *map_create(){
+    struct map *m = NULL;
+    struct skiplist *sl = NULL;
+
+    m = calloc(1, sizeof(*m));
+    if(NULL == m) goto err;
+    sl = skiplist_create(map_pair_cmp);
+    if(NULL == sl) goto err;
+    m->sl = sl;
+    return m;
+
+err:
+    if(NULL != sl)
+        skiplist_free(sl);
+    if(NULL != m)
+        free(m);
+    return NULL;
+}
+
+int map_del(struct map *m, void *key){
+    struct map_pair pair = { .key=key, .value=NULL };
+    struct map_pair *predeleted_pair = skiplist_remove(m->sl, (void *)&pair);
+    if(NULL != predeleted_pair){
+        map_pair_free(predeleted_pair);
+        return MAP_OK;
+    }
+    return MAP_ERR;
+}
+
+int map_put(struct map *m, struct map_pair *pair){
+    return SKIPLIST_OK == skiplist_insert(m->sl, (void *)pair) ? MAP_OK : MAP_ERR;
+}
+
+struct map_pair *map_get(struct map *m, void *key){
+    struct map_pair pair = { .key=key, .value=NULL };
+    return skiplist_search(m->sl, (void *)&pair);
+}
+
+void map_free(struct map *m){
+    if(NULL == m) return;
+    SKIPLIST_FOREACH(m->sl, (void *curr){
+        struct map_pair *pair = (struct map_pair *)curr;
+        //printf("key:%s value:%s\n", pair->key, pair->value);        
+        map_del(m, pair->key);
+    });
+    skiplist_free(m->sl);
+    free(m);
+}
+
+///////////////////////////////////////////////////////////////////////////////
+
+void stress_testing(struct map *m, int data_len, int count) __attribute__((unused));
+void stress_testing(struct map *m, int data_len, int count) {
+    int64_t start = getCurrentTime();
+    int i = 0;
+    for(; i < count; i++){
+        struct map_pair *pair = calloc(1, sizeof(*pair));
+        pair->key = random_str(data_len); //the key and value cannot be the same; otherwise, they will be released twice
+        pair->value = random_str(data_len);
+        assert(MAP_OK == map_put(m, pair));
+        assert(pair == map_get(m, pair->key));
+    }
+    printf("time consuming:%ld data_len:%d count:%d\n", getCurrentTime() - start, data_len, count);
+}
+
+void cover_testing(struct map *m) __attribute__((unused));
+void cover_testing(struct map *m) {
+
+    char *old_key = calloc(1, 5);
+    strcpy(old_key, "test");
+    char *old_value = calloc(1, 4);
+    strcpy(old_value, "old");
+    struct map_pair *old_pair = calloc(1, sizeof(*old_pair));
+    old_pair->key = old_key;
+    old_pair->value = old_value;
+
+    char *new_key = calloc(1, 5);
+    strcpy(new_key, "test");
+    char *new_value = calloc(1, 4);
+    strcpy(new_value, "new");
+    struct map_pair *new_pair = calloc(1, sizeof(*new_pair));
+    new_pair->key = new_key;
+    new_pair->value = new_value;
+
+    assert(MAP_OK == map_put(m, old_pair));
+    assert(MAP_ERR == map_put(m, new_pair)); //duplicate values are not allowed to be inserted
+    assert(old_pair == map_get(m, (void *)"test"));
+    map_pair_free(new_pair);
+}
+
+int main(){
+    struct map *m = map_create();
+
+    stress_testing(m, MAP_MAX_KEY_LEN, 100000);
+    cover_testing(m);
+
+    //free
+    printf("map size before free:%d\n", m->sl->busy);
+    map_free(m);
+
+    printf("over\n");
+    return 0;
+}

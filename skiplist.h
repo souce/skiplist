d59d9221d5bf8f414d57ab7b81bc3941fcfd88f8
joel <bin3340@163.com>
2021-12-04 18:38:59 +0800

    Update skiplist.h

--- skiplist.h
+++ skiplist.h
@@ -36,27 +36,21 @@ typedef int skiplist_cmp_item(void *k1, void *k2);
 struct skiplist{
     int busy;
     skiplist_cmp_item *cmp_item;
-    struct skiplist_node header[0]; //pointer to default_header
-    struct skiplist_node default_header;
+    struct skiplist_node header[1];
 };
 
 int skiplist_init(struct skiplist *sl, skiplist_cmp_item *cmp_item);
 struct skiplist_node *skiplist_search(struct skiplist *sl, struct skiplist_node *node);
 int skiplist_insert(struct skiplist *sl, struct skiplist_node *new_node);
 struct skiplist_node *skiplist_remove(struct skiplist *sl, struct skiplist_node *del_node);
-
-typedef int skiplist_traversal(struct skiplist_node *curr);
-#define SKIPLIST_FOREACH(sl, function_body) \
+#define SKIPLIST_FOREACH(sl, iter, loop_body) \
     do{ \
-        skiplist_traversal *traversal = ({ \
-            int __nested_func_ptr__ function_body \
-            __nested_func_ptr__; \
-        }); \
-        struct skiplist_node *node = (sl)->header->forward[0]; \
-        while(NULL != node){ \
-            struct skiplist_node *curr = node; \
-            node = node->forward[0]; \
-            if(SKIPLIST_OK != traversal(curr)) break; \
+        while(NULL != (iter)){ \
+            struct skiplist_node *iter_next = (iter)->forward[0]; \
+            if((sl)->header != (iter)){ \
+                (loop_body); \
+            } \
+            (iter) = iter_next; \
         }; \
     }while(0);
 
